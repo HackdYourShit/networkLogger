57aea6e6eda7d97b24198b8e636497a5ebf94e64
Abhishek Ravi <abhishekravi@InnovationM-30-Ravi.local>
2017-11-02 16:40:50 +0530

    Added WebClint

--- db.js
+++ db.js
@@ -2,9 +2,7 @@ var mongoClient = require('mongodb').MongoClient;
 var dateFormat = require('dateformat');
 
 var mongoDbURL = "mongodb://appx:appx123@ds117935.mlab.com:17935/db_networklogger";
-
 var collectionLogs = "Logs";
-
 
 mongoClient.connect(mongoDbURL, function(error, db){
 
@@ -17,28 +15,12 @@ mongoClient.connect(mongoDbURL, function(error, db){
 
   var currentDateTime = dateFormat(new Date(), "dddd, mmmm dS, yyyy, h:MM:ss TT");
 
-  //Insert
-  db.collection(collectionLogs).insertOne({
-    'timestamp': currentDateTime,
-    state: 'down',
-    downloadSpeed: '1.2',
-    uploadSpeed: '5.0'
-  });
-
   // Fetch
   var cursor = db.collection(collectionLogs).find();
 
   cursor.each(function(error, doc){
-
+  
     console.log(doc);
   });
 
 });
-
-/*
-db.collection(collectionLogs).insertOne({
-  state: 'active',
-  downloadSpeed: '4.2',
-  uploadSpeed: '10.2'
-});
-*/
