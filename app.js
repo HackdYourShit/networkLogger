b615f5e4c787ef3e35517704c2073066a5b8ae0c
Abhishek Ravi <abhishekravi@InnovationM-30-Ravi.local>
2017-11-02 15:31:27 +0530

    Change in MongoDb and DateFormatting

--- app.js
+++ app.js
@@ -1,26 +1,85 @@
 var speedTest = require('speedtest-net');
+var mongoClient = require('mongodb').MongoClient;
+var dateFormat = require('dateformat');
 
+// Speed Test Config
 var test = speedTest({maxTime: 5000});
 
-test.on('testserver', function(server){
-  pingTime = server.bestPing;
-});
+// MongoLab Config
+var mongoDbURL = "mongodb://appx:appx123@ds117935.mlab.com:17935/db_networklogger";
+var collectionLogs = "Logs";
 
-test.on('data', function(data){
+// Local Fields
+var downloadSpeed = "";
+var uploadSpeed = "";
+var internetProvider = "";
+var internetProviderIP = "";
 
-  var downloadSpeed = data.speeds.download + ' Mbps';
-  var uploadSpeed = data.speeds.upload + ' Mbps';
+// Call Network Test
+networkTesting();
 
-  console.log("Download Speed ", downloadSpeed);
-  console.log("Upload Speed ", uploadSpeed);
-  console.log("ISP ", data.client.isp);
-  console.log("ISP ", data.client.ip);
-  console.log("Latitude ", data.client.lat);
-  console.log("Longitude ", data.client.lon);
+function networkTesting() {
 
-});
+  test.on('testserver', function(server){
+    pingTime = server.bestPing;
+  });
 
-test.on('error', function(error){
+  test.on('data', function(data){
 
-  console.log(error);
-});
+    downloadSpeed = data.speeds.download + ' Mbps';
+    uploadSpeed = data.speeds.upload + ' Mbps';
+    internetProvider = data.client.isp;
+    internetProviderIP = data.client.ip;
+
+    console.log("Download Speed ", downloadSpeed);
+    console.log("Upload Speed ", uploadSpeed);
+    console.log("ISP ", internetProvider);
+    console.log("ISP ", internetProviderIP);
+    console.log("Latitude ", data.client.lat);
+    console.log("Longitude ", data.client.lon);
+
+    // Connect to mongodb
+    logToMongoDb();
+  });
+
+  test.on('error', function(error){
+    console.log(error);
+  });
+
+}
+
+
+function logToMongoDb() {
+
+  mongoClient.connect(mongoDbURL, function(error, db){
+
+    if(error){
+      return console.log("Db Connection :"+error);
+    }
+
+    //onSuccess
+    console.log("Connected to MongoDb");
+
+    // Current Date&Time
+    var currentDateTime = dateFormat(new Date(), "dddd, mmmm dS, yyyy, h:MM:ss TT");
+
+    //Insert
+    db.collection("Logs").insertOne({
+      timeStamp: currentDateTime,
+      state: 'UP',
+      internetProvider: internetProvider,
+      internetIP: internetProviderIP,
+      downloadSpeed: downloadSpeed,
+      uploadSpeed: uploadSpeed
+    }, function(error, response){
+
+      if(error){
+        return console.log("Record Insertion : "+error);
+      }
+
+      //Successfully Created
+      console.log("Record Instered to Db :)");
+    });
+
+  });
+}
--- db.js
+++ db.js
@@ -1,8 +1,10 @@
 var mongoClient = require('mongodb').MongoClient;
+var dateFormat = require('dateformat');
 
 var mongoDbURL = "mongodb://appx:appx123@ds117935.mlab.com:17935/db_networklogger";
 
 var collectionLogs = "Logs";
+
 
 mongoClient.connect(mongoDbURL, function(error, db){
 
@@ -13,14 +15,16 @@ mongoClient.connect(mongoDbURL, function(error, db){
   //onSuccess
   console.log("Connected to Db.");
 
+  var currentDateTime = dateFormat(new Date(), "dddd, mmmm dS, yyyy, h:MM:ss TT");
+
   //Insert
-  /*
   db.collection(collectionLogs).insertOne({
+    'timestamp': currentDateTime,
     state: 'down',
     downloadSpeed: '1.2',
     uploadSpeed: '5.0'
   });
-*/
+
   // Fetch
   var cursor = db.collection(collectionLogs).find();
 
